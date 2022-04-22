d88d829ecc4d39eeae417df2352803a1638de95b
R4sp1 <57303554+R4sp1@users.noreply.github.com>
2022-04-22 12:24:29 +0200

    Added function to seed database when its empty

---
+++ Project/Models/SeedData.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Project.Data;
+using System;
+using System.Linq;
+
+namespace Project.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new ProjectContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<ProjectContext>>()))
+            {
+                // Look for any Others parts
+                if (context.Others.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Others.AddRange(
+                    new Others
+                    {
+                        Name = "Omron g5v-2-h1",
+                        Type = "Relay",
+                        Quantity = 1,
+                        Price = 59
+                    },
+
+                    new Others
+                    {
+                        Name = "Winstar WG12864B-GFH-V",
+                        Type = "Display",
+                        Quantity = 2,
+                        Price = 646
+                    },
+
+                    new Others
+                    {
+                        Name = "HC49U - 1,843 MHz",
+                        Type = "Krystal",
+                        Quantity = 100,
+                        Price = 17
+                    },
+
+                    new Others
+                    {
+                        Name = "TEC1-127090S - 84 W",
+                        Type = "Peltier cooler",
+                        Quantity = 50,
+                        Price = 300
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- Project/Program.cs
+++ Project/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -6,6 +7,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Project.Data;
+using Project.Models;
 
 namespace Project
 {
@@ -13,7 +16,24 @@ namespace Project
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
